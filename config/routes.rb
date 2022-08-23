0f3a05960ca9584b7513f82974d89be034cbeace
Edd Shotton <edward.shotton@gmail.com>
2022-08-23 16:55:09 +0100

    update complete

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,14 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "tasks", to: "tasks#index"
+  # Create
   get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  # Update
+  get "tasks/:id/edit", to: "tasks#edit"
+  patch "tasks/:id", to: "tasks#update"
+   
   # The id needs to be at the bottom
   get "tasks/:id", to: "tasks#show", as: :task
+
 end
